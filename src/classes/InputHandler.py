c90c3edd62ea78c391fa76ce6e8004dde8892310
Nicosator94 <nicolas.romanoff94@gmail.com>
2025-07-04 20:37:15 +0200

    refacto: change how invoke be call with better management of the dictionary of functions and parameters

--- src/classes/InputHandler.py
+++ src/classes/InputHandler.py
@@ -1,5 +1,6 @@
 from ursina import Entity, application, invoke, curve, held_keys
 from .Rubik3D import Rubik3D
+from .Position import PRIME, DOUBLE
 
 
 class InputHandler(Entity):
@@ -33,34 +34,22 @@ class InputHandler(Entity):
 
     def do_mix(self):
         self.is_mix = True
-        dictionnary = {
-            "F": lambda: self.do_front(False),
-            "F\'": lambda: self.do_front(True),
-            "F’": lambda: self.do_front(True),
-            "F2": lambda: self.do_front(False, True),
-            "B": lambda: self.do_back(False),
-            "B\'": lambda: self.do_back(True),
-            "B’": lambda: self.do_back(True),
-            "B2": lambda: self.do_back(False, True),
-            "U": lambda: self.do_top(False),
-            "U\'": lambda: self.do_top(True),
-            "U’": lambda: self.do_top(True),
-            "U2": lambda: self.do_top(False, True),
-            "D": lambda: self.do_bottom(False),
-            "D\'": lambda: self.do_bottom(True),
-            "D’": lambda: self.do_bottom(True),
-            "D2": lambda: self.do_bottom(False, True),
-            "L": lambda: self.do_left(False),
-            "L\'": lambda: self.do_left(True),
-            "L’": lambda: self.do_left(True),
-            "L2": lambda: self.do_left(False, True),
-            "R": lambda: self.do_right(False),
-            "R\'": lambda: self.do_right(True),
-            "R’": lambda: self.do_right(True),
-            "R2": lambda: self.do_right(False, True),
+        func_dict = {
+            "F": self.do_front,
+            "B": self.do_back,
+            "U": self.do_top,
+            "D": self.do_bottom,
+            "L": self.do_left,
+            "R": self.do_right,
         }
         for i in range(len(self.rubik3D.rubik.mix)):
-            invoke(dictionnary[self.rubik3D.rubik.mix[i]], delay=i * self.time)
+            func = func_dict[self.rubik3D.rubik.mix[i][0]]
+            reverse = False
+            double = False
+            if len(self.rubik3D.rubik.mix[i]) == 2:
+                reverse = True if self.rubik3D.rubik.mix[i][1] in PRIME else False
+                double = True if self.rubik3D.rubik.mix[i][1] is DOUBLE else False
+            invoke(func, reverse, double, delay=i * self.time)
         invoke(lambda: setattr(self, "is_mix", False), delay=len(self.rubik3D.rubik.mix))
 
     def do_rotate(self, type, value):
@@ -68,7 +57,7 @@ class InputHandler(Entity):
         invoke(self.rubik3D.clear_rubik, delay=self.time)
         invoke(lambda: setattr(self, "is_input", False), delay=self.time)
 
-    def do_front(self, reverse, double=False):
+    def do_front(self, reverse, double):
         self.is_input = True
         z = 0
         for y in range(3):
@@ -80,7 +69,7 @@ class InputHandler(Entity):
         self.do_rotate("rotation_z", round(self.rubik3D.center.rotation_z) +
                        ((-90 if reverse else 90) * (2 if double else 1)))
 
-    def do_back(self, reverse, double=False):
+    def do_back(self, reverse, double):
         self.is_input = True
         z = 2
         for y in range(3):
@@ -92,7 +81,7 @@ class InputHandler(Entity):
         self.do_rotate("rotation_z", round(self.rubik3D.center.rotation_z) +
                        ((90 if reverse else -90) * (2 if double else 1)))
 
-    def do_top(self, reverse, double=False):
+    def do_top(self, reverse, double):
         self.is_input = True
         y = 0
         for x in range(3):
@@ -104,7 +93,7 @@ class InputHandler(Entity):
         self.do_rotate("rotation_y", round(self.rubik3D.center.rotation_y) +
                        ((-90 if reverse else 90) * (2 if double else 1)))
 
-    def do_bottom(self, reverse, double=False):
+    def do_bottom(self, reverse, double):
         self.is_input = True
         y = 2
         for x in range(3):
@@ -116,7 +105,7 @@ class InputHandler(Entity):
         self.do_rotate("rotation_y", round(self.rubik3D.center.rotation_y) +
                        ((90 if reverse else -90) * (2 if double else 1)))
 
-    def do_left(self, reverse, double=False):
+    def do_left(self, reverse, double):
         self.is_input = True
         x = 0
         for z in range(3):
@@ -128,7 +117,7 @@ class InputHandler(Entity):
         self.do_rotate("rotation_x", round(self.rubik3D.center.rotation_x) +
                        ((90 if reverse else -90) * (2 if double else 1)))
 
-    def do_right(self, reverse, double=False):
+    def do_right(self, reverse, double):
         self.is_input = True
         x = 2
         for z in range(3):

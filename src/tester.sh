0df8b7318d387d432d1991ea5a5d8da2e5980ed9
MatHoyer <mathieuhoyer@gmail.com>
2025-07-25 20:31:32 +0200

    fix: remove mix print + tester handle error

--- src/tester.sh
+++ src/tester.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-ITERATIONS=100
+ITERATIONS=10
 global_count=0
 exec_count=0
 
 for ((i=0; i<ITERATIONS; i++)); do
-    ((exec_count++))
-    count=$(python src/rubik.py -r | wc -l)
-    ((global_count+=count))
+    # Capture both stdout and stderr, check exit status
+    if output=$(python src/rubik.py -r 2>>/tmp/error.tmp); then
+        count=$(echo "$output" | wc -l)
+        ((global_count+=count))
+        ((exec_count++))
+    else
+        echo "Iteration $((i+1)): Python execution failed with exit code $?" >> error.log
+        cat /tmp/error.tmp >> error.log
+        rm /tmp/error.tmp
+        echo "" >> error.log
+    fi
 done
 
-echo $(echo "scale=2; $global_count / $exec_count" | bc)
+echo "Moyenne: $(((global_count / exec_count)))"
